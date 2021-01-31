93fab7a1a99396699cb5d06fb93482061175cce7
allen3_huang <allen3_huang@asus.com>
2021-01-31 09:36:13 +0800

    modify

--- main.go
+++ main.go
@@ -34,17 +34,19 @@ func Get(c *gin.Context) {
 // 取得單一筆資料
 func GetOne(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
+
 	if err != nil {
-		c.JSON(http.StatusBadRequest, "param error!")
+		c.JSON(http.StatusBadRequest, "param id error!")
 		return
 	}
+
 	for _, role := range model.Roles {
 		if role.ID == uint(id) {
 			c.JSON(http.StatusOK, role)
 			return
 		}
 	}
-	c.JSON(http.StatusNotFound, nil)
+	c.JSON(http.StatusNotFound, "No Find Data!")
 	return
 }
 
@@ -55,7 +57,7 @@ func Post(c *gin.Context) {
 	err := decoder.Decode(&role)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, "param error!")
+		c.JSON(http.StatusBadRequest, "param body error!")
 		return
 	}
 
@@ -90,25 +92,34 @@ func Put(c *gin.Context) {
 		}
 	}
 
-	c.JSON(http.StatusNotFound, nil)
+	c.JSON(http.StatusNotFound, "No Find Modify Data!")
 	return
 }
 
 // 刪除資料
 func Delete(c *gin.Context) {
-	newid := c.Param("newid")
+	strNewid := c.Param("id")
 
-	if newid, err := strconv.Atoi(newid); err == nil {
-		unewid := uint(newid)
+	newid, err := strconv.Atoi(strNewid)
 
-		for i := 0; i < len(model.Roles); i++ {
-			if model.Roles[i].ID == unewid {
-				model.Roles = append(model.Roles[0:i], model.Roles[i+1:]...)
-			}
-		}
-		c.JSON(http.StatusNotFound, model.Roles)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "param id error!")
 		return
 	}
+
+	for i := 0; i < len(model.Roles); i++ {
+		if model.Roles[i].ID == uint(newid) {
+			if i == len(model.Roles)-1 {
+				model.Roles = model.Roles[0:i]
+			} else {
+				model.Roles = append(model.Roles[0:i], model.Roles[i+1:]...)
+			}
+			c.JSON(http.StatusOK, "Delete ok")
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, "No Find Delete Data!")
+	return
 
 }
 
